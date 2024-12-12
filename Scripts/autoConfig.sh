927a542a4324988a62da7d2d32fe60adce17d7d3
AL-R57 <130043683+AL-R57@users.noreply.github.com>
2024-12-12 17:39:49 +0100

    Update autoConfig.sh

--- Scripts/autoConfig.sh
+++ Scripts/autoConfig.sh
@@ -3,12 +3,13 @@
 # Expected NAME in argument of this script
 
 NAME=$1
+LAN_IP=$2
 
+scp /doc/config.sh root@$LAN_IP:/tmp/
 ssh root@$LAN_IP
-scp config.sh /tmp/
-scp configWifi.sh /tmp/
-./config.sh $NAME
-./configWifi.sh $NAME
+PASSWD
+chmod +x /tmp/config.sh
+/tmp/config.sh $NAME
 
 echo "
 
