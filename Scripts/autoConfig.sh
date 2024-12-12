bfba9711c82dd5d3598887ba1e9d2d3b17a58ae7
AL-R57 <130043683+AL-R57@users.noreply.github.com>
2024-12-12 17:41:56 +0100

    Update autoConfig.sh

--- Scripts/autoConfig.sh
+++ Scripts/autoConfig.sh
@@ -11,6 +11,10 @@ PASSWD
 chmod +x /tmp/config.sh
 /tmp/config.sh $NAME
 
-echo "
-
+echo "Configuration mise à jour sur $NAME:"
+echo "- IPv4 basculé en DHCP."
+echo "- IPv6 statique configuré (fd71:C:2025$IPID/64)."
+echo "- Adresse IPv4 '$LAN_IP' supprimée (si existante)."
+echo "- Mot de passe root modifié."
+echo "Script fonctionnel sans erreur"
 
