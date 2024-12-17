cf6b4d05fe1d6b6a56faab37d566a97160727c69
Lucumerry <145458440+Arlausea@users.noreply.github.com>
2024-12-17 14:46:25 +0100

    Update configforFlash.sh

--- Scripts/configforFlash.sh
+++ Scripts/configforFlash.sh
@@ -1,13 +1,13 @@
 #!/bin/sh
 
-# Nom et IP (à préciser en paramètres)
+# Nom et IP (à préciser en arguments)
 
-NAME="Lapin_01"
+NAME=Lapin_01
 ID="${NAME: -2}"
 IPID="::${ID}0"
-IP="192.168.1.1/24" # Adresse par défaut sur OpenWirt
-
-
+IP="192.168.2.${ID}0/24"
+PASSWD="theseus-2023"
+NAMEID="Rabbit_${ID}_5GHz"
 
 # Nouveau mot de passe root
 NEW_ROOT_PASSWD="theseus-2023"
@@ -40,11 +40,40 @@ echo "Mise à jour des paramètres network en cours."
 uci commit network
 /etc/init.d/network restart
 
-echo "Configuration mise à jour :"
-echo "- IPv4 basculé en DHCP."
-echo "- IPv6 statique configuré (fd71:C:2025$IPID/64)."
-echo "- Adresse IPv4 '$IP' supprimée (si existante)."
-echo "- Mot de passe root modifié."
-echo "Script fonctionnel sans erreur"
 
 
+uci set wireless.@wifi-device[0].disabled='0'
+uci set wireless.@wifi-device[1].disabled='0'
+
+# Configurer le wifi mesh en 2.4 GHz
+uci add wireless wifi-iface
+uci set wireless.@wifi-iface[-1].device='radio1'  # Associer à la radio1
+uci set wireless.@wifi-iface[-1].mode='mesh'  # Mode mesh
+uci set wireless.@wifi-iface[-1].encryption='sae'  # Chiffrement WPA3-SAE (recommandé pour le mesh)
+uci set wireless.@wifi-iface[-1].key=$PASSWD  # Clé de sécurité
+uci set wireless.@wifi-iface[-1].mesh_id='THESEUS'  # Nom du réseau maillé
+uci set wireless.@wifi-iface[-1].mesh_fwding='1'  # Activer le forwarding dans le mesh
+uci set wireless.@wifi-iface[-1].mesh_rssi_threshold='0'  # Aucun seuil RSSI (par défaut)
+uci set wireless.@wifi-iface[-1].network='lan'  # Associer au réseau 'lan'
+
+# Appliquer les changements
+uci commit wireless
+wifi reload
+
+# Configurer le point d'accès en 5Ghz
+
+uci add wireless wifi-iface
+uci set wireless.@wifi-iface[-1].device='radio0'
+uci set wireless.@wifi-iface[-1].mode='ap'
+uci set wireless.@wifi-iface[-1].ssid=$NAMEID
+uci set wireless.@wifi-iface[-1].encryption='sae-mixed'  
+uci set wireless.@wifi-iface[-1].key=$PASSWD  
+uci set wireless.@wifi-iface[-1].ieee80211r='1'
+uci set wireless.@wifi-iface[-1].mobility_domain='2222'
+uci set wireless.@wifi-iface[-1].ft_over_ds='0'
+uci set wireless.@wifi-iface[-1].ft_psk_generate_local='1'
+uci set wireless.@wifi-iface[-1].network='lan'  
+
+
+uci commit wireless
+wifi reload
